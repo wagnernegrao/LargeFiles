0c805b6d5c1dbae02e324dfc07425659982601a0
wagnernegrao <wagnerfelidre@gmail.com>
2020-02-21 18:05:25 -0300

    add folder to add files used in diff

---
+++ Tools/diff_commit/script_git_log.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INPUT=lista_de_diretorios2.csv
+OLDIFS=$IFS
+IFS=','
+
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read project name dir
+do
+    git git log --oneline -M --stat --follow --pretty="%aI, %s, %an, %ae" -- $dir > /home/wagner/GitHub/files_txt/$project-$name.txt
+	echo "Project: $project"
+    echo "Name : $name"
+	echo "Dir : $dir"
+
+    echo "Test: /home/wagner/GitHub/files_txt/$project-$name.txt"
+done < $INPUT
+IFS=$OLDIFS
