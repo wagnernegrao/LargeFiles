4321420bf9db86e288761bff8e0de184a526245e
wagnernegrao <wagnerfelidre@gmail.com>
2020-02-21 18:39:35 -0300

    fix bug

--- Tools/diff_commit/script_git_log.sh
+++ Tools/diff_commit/script_git_log.sh
@@ -5,9 +5,9 @@ OLDIFS=$IFS
 IFS=','
 
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read project name dir
+while read project name dirr
 do
-    git git log --oneline -M --stat --follow --pretty="%aI, %s, %an, %ae" -- $dir > /home/wagner/GitHub/files_txt/$project-$name.txt
+    git git log --oneline -M --stat --follow --pretty="%aI, %s, %an, %ae" -- $dirr > /home/wagner/GitHub/files_txt/$project-$name.txt
 	echo "Project: $project"
     echo "Name : $name"
 	echo "Dir : $dir"
