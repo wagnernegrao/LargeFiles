ae1b92c53eee15b74c6e618b14c5982fc641f404
wagnernegrao <wagnerfelidre@gmail.com>
2020-03-27 17:31:48 -0300

    add documentacao

--- Tools/growth_curve/git_history_log.sh
+++ Tools/growth_curve/git_history_log.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Para cada pasta no endereco citado entra na pasta e executa o comando do git
+# e salva no diretorio passado
+
 for arq in `ls /home/wagnernegrao/projetos_large_files/`; do
     cd /home/wagnernegrao/projetos_large_files/$arq
     git log --oneline > /home/wagnernegrao/Documents/dados_growth_curve/log_projeto/$arq.txt
