fe3bc1742222767019c29a17ae91662bf527826b
wagnernegrao <wagnerfelidre@gmail.com>
2020-03-27 17:31:33 -0300

    add documentacao

--- Tools/growth_curve/get_cloc.sh
+++ Tools/growth_curve/get_cloc.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
+# Argumentos que serao passados
 name="$1"
 commit="$2"
 
 
-# echo "rodou => $name ==> $commit"
-
-
+# Entra na pasta e executa os comandos do git e dps sai da pasta e roda o cloc
 cd /home/wagnernegrao/projetos_large_files/$name
 git reset --hard $commit
 date_git=$(git show -s --format=%ci $commit)
 date="$(cut -d' ' -f1 <<<"$date_git")"
 
-echo "$date"
-
 cd ..
-
 
 cloc --by-file /home/wagnernegrao/projetos_large_files/$name --csv --out=/home/wagnernegrao/Documents/dados_growth_curve/dados_csv_cloc/$name*$commit*$date.csv
