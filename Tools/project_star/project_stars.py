2743282dae509da4fae04167708360ff544b81f4
wagnernegrao <wagnerfelidre@gmail.com>
2020-03-27 17:24:24 -0300

    add documentacao

--- Tools/project_star/project_stars.py
+++ Tools/project_star/project_stars.py
@@ -1,34 +1,36 @@
 import pandas as pd
 from github import Github
 
-user = ''
-password = ''
+user = ""
+password = ""
 
+filename = "directory-path to CSV"
 
-filename = 'directory-path to CSV'
-df = pd.read_csv(filename, sep=',')
+# Le um csv com de projetos e o criador
+df = pd.read_csv(filename, sep=",")
+
+# adiciona tudo em uma lista
 link_list = df.name.tolist()
 
-
-
-
+# cria um objeto
 g = Github(user, password)
 
 project_stars = []
 
+# Percorre cada link da lista
 for link in link_list:
-	try:
-		# print(f'\nProjeto: {link}')
-		repo = g.get_repo(link)
+    try:
+        # Obten a informacao do repositorio
+        repo = g.get_repo(link)
 
-		# print(f'Quantidade de estrelas: {repo.stargazers_count}\n\n')
-		project_stars.append(repo.stargazers_count)
-	except:
-		print(f'\n\nErro no projeto: {link}')
+        # Adiciona na lista a quantiade de estrelas
+        project_stars.append(repo.stargazers_count)
+    except:
+        print(f"\n\nErro no projeto: {link}")
 
-
+# Cria uma serie
 stars = pd.Series(project_stars)
 
 
-print(f'\nQuantidade de estrelas: ')
+print(f"\nQuantidade de estrelas: ")
 print(stars.describe())
